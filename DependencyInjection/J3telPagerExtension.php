a4cd300cc37ffb73f795272d3e0b692274e3c5b2
Aristide <asoullard@aso-PC.j3tel.adds>
2014-08-08 11:31:05 +0200

    renommage fichie r

--- DependencyInjection/J3telPagerExtension.php
+++ DependencyInjection/J3telPagerExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class J3telCoreExtension extends Extension
+class J3telPagerExtension extends Extension
 {
     /**
      * {@inheritDoc}
